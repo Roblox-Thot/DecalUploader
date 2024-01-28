fca3776dbcefc4d02e4a0e09b391456d06ad1688
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-01-27 16:01:51 -0800

    Update DecalUploader.py

--- DecalUploader.py
+++ DecalUploader.py
@@ -7,17 +7,17 @@ from bs4 import BeautifulSoup
 class DecalClass():
     global classes
     classes = {
-        "Audio": 3,
-        "Mesh": 4,
-        "Lua": 5,
-        "Model": 10,
-        "Decal": 13,
-        "Gear": 19,
-        "Plugin": 38,
-        "MeshPart": 40
+        'Audio': 3,
+        'Mesh': 4,
+        'Lua': 5,
+        'Model': 10,
+        'Decal': 13,
+        'Gear': 19,
+        'Plugin': 38,
+        'MeshPart': 40
     }
 
-    def __init__(self, cookie:str, location:str, name:str, description:str = "Studio", type:str = "Decal"):
+    def __init__(self, cookie:str, location:str, name:str, description:str = 'Studio', type:str = 'Decal'):
         """Set up the DecalClass
 
         Args:
@@ -29,7 +29,7 @@ class DecalClass():
         self.uploadType = classes[type]
         self.request = requests.Session() # Make a request session so its easier later
         self.request.cookies.update({'.ROBLOSECURITY': cookie}) # Setting ROBLOSECURITY cookie
-        self.request.headers.update({"User-Agent": "RobloxStudio/WinInet RobloxApp/0.601.0.6010507 (GlobalDist; RobloxDirectDownload)"})# Sets a the UA to the Roblox Studio
+        self.request.headers.update({'User-Agent': 'RobloxStudio/WinInet RobloxApp/0.609.0.6090385 (GlobalDist; RobloxDirectDownload)'})# Sets a the UA to the Roblox Studio
         self.location = location
         self.uploadURL = f'https://data.roblox.com/data/upload/json?assetTypeId={self.uploadType}&name={urllib.parse.quote(name)}&description={urllib.parse.quote(description)}'
 
@@ -39,11 +39,11 @@ class DecalClass():
         Returns:
             string: CSRF token header
         """
-        if token := self.request.post("https://auth.roblox.com/v2/login").headers[
+        if token := self.request.post('https://auth.roblox.com/v2/login').headers[
             'x-csrf-token'
         ]:
             return token
-        print("Please log in (X-csrf-token fail)")
+        print('Please log in (X-csrf-token fail)')
         exit() # Lazy hack mate
     
     def upload(self):
@@ -64,12 +64,13 @@ class DecalClass():
         with open(self.location, 'rb') as dick: # Open image as bytes
             data = dick.read() + os.urandom(69)
 
-        headers = {"Requester": "Client",
-                    "X-CSRF-TOKEN": token
+        headers = {
+                    'Requester': 'Client',
+                    'X-CSRF-TOKEN': token
                 }
         PData = self.request.post(self.uploadURL,headers=headers,data=data)
-        if "Re-activate My Account" in PData.text:
-            print("Warned attempting to fix")
+        if 'Re-activate My Account' in PData.text:
+            print('Warned attempting to fix')
             try:
                 # Get the form data to post
                 soup = BeautifulSoup(PData.content, 'html.parser')
@@ -77,46 +78,49 @@ class DecalClass():
                 punishmentId = soup.find('input', {'name': 'punishmentId'})['value']
 
                 # Post the data
-                self.request.post("https://www.roblox.com/not-approved/reactivate", data={'__RequestVerificationToken':verification_token,'punishmentId':punishmentId}, headers={'Content-Type':'application/x-www-form-urlencoded'})
+                self.request.post('https://www.roblox.com/not-approved/reactivate', data={'__RequestVerificationToken':verification_token,'punishmentId':punishmentId}, headers={'Content-Type':'application/x-www-form-urlencoded'})
 
                 PData = self.upload()
             except:
-                input("failed to reactivate")
-        elif "not-approved" in PData.url:
-            input("banned/warned pls check (press enter to retry upload)") # Pause... are you banned/warned... thats cringe
+                input('failed to reactivate')
+        elif 'not-approved' in PData.url:
+            input('banned/warned pls check (press enter to retry upload)') # Pause... are you banned/warned... thats cringe
             PData = self.upload() # Retry upload after the user hits enter
         elif 'retry-after' in PData.headers:
-            pausetime = int(PData.headers["retry-after"]) + 1 # Get the retry header and wait 1 extra sec
+            pausetime = int(PData.headers['retry-after']) + 1 # Get the retry header and wait 1 extra sec
             print(f'Rate limited for {pausetime} seconds... (Waiting)')
             sleep(pausetime)
             PData = self.upload() # Retry upload after Rate limit (:skull:)
         return PData
 
-if "__main__" in __name__:
+if '__main__' in __name__:
     import os
     import sys
-    if len(sys.argv) < 2:
-        ROBLOSECURITY = input("Cookie: ")
-    else:
-        ROBLOSECURITY = sys.argv[1]
+    try:
+        ROBLOSECURITY = open('cookie.txt').readline() # Because this fucker is so lazy ig https://v3rm.net/threads/roblox-decal-tools.144/post-2702
+    except:
+        if len(sys.argv) < 2:
+            ROBLOSECURITY = input('Cookie: ')
+        else:
+            ROBLOSECURITY = sys.argv[1]
 
-    clear = input("Clear Out.csv? (Y/N): ")
-    if "y" in clear.lower():
-        with open("Out.csv",'w') as clr:
-            clr.write("FileName,DecalId,ImageId\n") # CSV headers
+    clear = input('Clear Out.csv? (Y/N): ')
+    if 'y' in clear.lower():
+        with open('Out.csv','w') as clr:
+            clr.write('FileName,DecalId,ImageId\n') # CSV headers
 
     directory = 'files'
     for filename in os.listdir(directory):
         try:
             f = os.path.join(directory, filename) # get the img path
-            a = DecalClass(ROBLOSECURITY, f, os.urandom(2), "I love it").upload().json() # Create the upload and upload then get the json data
-            if a["Success"]:
-                with open("Out.csv",'a') as out:
+            a = DecalClass(ROBLOSECURITY, f, os.urandom(2), 'Uploaded with studio').upload().json() # Create the upload and upload then get the json data
+            if a['Success']:
+                with open('Out.csv','a') as out:
                     out.write(f'{filename},{a["AssetId"]},{a["BackingAssetId"]}\n')
                 print(f'Uploaded {filename} (AssetID: {a["AssetId"]})')
                 os.remove(f)
             else:
-                print(filename, a["Message"])
+                print(filename, a['Message'])
             sleep(randint(0,2)) # Give Roblox a random break
         except:
             pass # something somewhere is broken idc to debug rn
